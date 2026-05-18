fe6aee182eece6d3cc81571fe466045a2ea401f3
William Belle <william.belle@gmail.com>
2026-05-18 10:05:46 +0200

    Add Web2018 templates

---
+++ tests/test_templates.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class TestTemplates(TestCase):
+
+    def test_home(self):
+        response = self.client.get("/")
+        self.assertEqual(200, response.status_code)
+        self.assertIn(
+            "<title>Test Home - EPFL</title>",
+            response.content.decode(),
+        )
+        self.assertIn(
+            "Test App",
+            response.content.decode(),
+        )
